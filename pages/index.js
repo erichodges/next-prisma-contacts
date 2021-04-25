9a03e58ef7e5788b8ac9ae4a1225c6aa308e5338
Eric Hodges <ehodges2004@gmail.com>
2021-04-25 15:08:34 -0700

    Fixed typo on index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return (
-    
-  )
+  return <div></div>;
 }
